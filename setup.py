fb7ffc66101181e473423f3e2b0a4653d65a243a
William Schoenell <wschoenell@gmail.com>
2015-07-03 15:33:41 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='chimera-serial',
+    name='chimera-jmismart232',
     version='0.0.1',
-    packages=['chimera_serial', 'chimera_serial.instruments'],
+    packages=['chimera_jmismart232', 'chimera_jmismart232.instruments'],
     scripts=[],
-    url='http://github.com/astroufsc/chimera-serial',
+    url='http://github.com/astroufsc/chimera-jmismart232',
     license='GPL v2',
     author='William Schoenell',
     author_email='william@iaa.es',
     install_requires=['pyserial'],
-    description='Chimera plugin for serial devices'
+    description='Chimera plugin for JMI Smart 232 serial focusers'
 )
